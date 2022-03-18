afcd0eda9543057b70b1e5fbebb16501eff44870
Yannick <ybroeks11@gmail.com>
2022-03-18 13:53:56 +0100

    Enum working!

--- Assets/yeet/ChangeTexture.cs
+++ Assets/yeet/ChangeTexture.cs
@@ -4,35 +4,21 @@ using UnityEngine;
 
 public class ChangeTexture : MonoBehaviour
 {
-    public Texture[] usableTextures;
-    public GameObject objectChange;
-    public Renderer objectRenderer;
 
+
+
+    private void Start()
+    {
+
+        
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            CheckObject();
-        }
-
-        for (int i = 0; i < usableTextures.Length; i++)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha0 + 1 + i))
-            {
-                objectRenderer.material.mainTexture = usableTextures[i];
-            }
-        }
+  
     }
 
-    private void CheckObject()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            objectChange = hit.transform.gameObject;
-            objectRenderer = objectChange.GetComponent<Renderer>();
-        }
-    }
+
+
+
 }
--- Assets/yeet/Create.cs
+++ Assets/yeet/Create.cs
@@ -1,43 +1,96 @@
 using System;
 using UnityEngine;
 
+
+public enum ToolState
+{
+    CreateObject,
+    ChangeTexture,
+    MoveObject,
+    GroupObject
+}
+
 [ExecuteInEditMode]
 public class Create : MonoBehaviour, ICommandHandler
 {
+    public ToolState toolState;
+
+    [Header("Create Object Variables")]
     [SerializeField]
     private GameObject[] placeableObjectPrefabs;
-
-    private GameObject currentPlaceableObject;
+    private GameObject   currentPlaceableObject;
 
     private float xMouseWheelRotation;
     private float yMouseWheelRotation;
     private float zMouseWheelRotation;
 
-    private int currentPrefabIndex = -1;
+    private int   currentPrefabIndex = -1;
+    private float scrollSpeed = 0.1f;
 
-    public bool xAxisBool;
-    public bool yAxisBool;
-    public bool zAxisBool;
+    private bool xAxisBool;
+    private bool yAxisBool;
+    private bool zAxisBool;
 
+    [Header("Change Texture Variables")]
+    public Texture[]  usableTextures;
+    public GameObject objectChange;
+    public Renderer   objectRenderer;
 
-    public float scrollSpeed = 0.1f;
+    [Header("Move Object Variables")]
+    private int temp;
 
+    [Header("Group Object Variables")]
+    private int temp2;
+
+    
     public void Execute()
     {
     }
 
     public void Undo()
     {
-
     }
-
+    
     private void Update()
     {
-        HandleNewObjectHotkey();
-        ExecuteTasks();
+        switch (toolState)
+        {
+            case ToolState.CreateObject:
+                PlaceObjectTotal();
+                break;
+            case ToolState.ChangeTexture:
+                ChangeTextureTotal();
+                break;
+            case ToolState.MoveObject:
+
+                break;
+            case ToolState.GroupObject:
+
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            toolState = ToolState.CreateObject;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            toolState = ToolState.ChangeTexture;
+        }
     }
 
-    private void ExecuteTasks()
+    private void ChangeTextureTotal()
+    {
+        ChangeTexture();
+        CheckObject();
+    }
+    private void PlaceObjectTotal()
+    {
+        HandleNewObjectHotkey();
+        ExecuteTasksPlaceObject();
+    }
+
+    private void ExecuteTasksPlaceObject()
     {
         if (currentPlaceableObject != null)
         {
@@ -45,29 +98,22 @@ public class Create : MonoBehaviour, ICommandHandler
             RotateFromMouseWheel();
             ReleaseIfClicked();
         }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
+        if (Input.GetKeyDown(KeyCode.X)) {
             xAxisBool = true;
             yAxisBool = false;
             zAxisBool = false;
         }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
+        if (Input.GetKeyDown(KeyCode.Y)) {
             xAxisBool = false;
             yAxisBool = true;
             zAxisBool = false;
         }
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
+        if (Input.GetKeyDown(KeyCode.Z)) {
             xAxisBool = false;
             yAxisBool = false;
             zAxisBool = true;
         }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ResetRotation();
-        }
+        if (Input.GetKeyDown(KeyCode.R)) ResetRotation();
     }
 
     private void HandleNewObjectHotkey()
@@ -87,18 +133,14 @@ public class Create : MonoBehaviour, ICommandHandler
                     {
                         Destroy(currentPlaceableObject);
                     }
-
                     currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
                     currentPrefabIndex = i;
                     currentPlaceableObject.layer = 2;
-
                 }
                 break;
             }
         }
-    }
-
-    
+    }   
 
     private bool PressedKeyOfCurrentPrefab(int i)
     {
@@ -154,6 +196,33 @@ public class Create : MonoBehaviour, ICommandHandler
         {
             currentPlaceableObject.layer = 0;
             currentPlaceableObject = null;
+        }
+    }
+
+    public void ChangeTexture()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            CheckObject();
+        }
+
+        for (int i = 0; i < usableTextures.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + 1 + i))
+            {
+                objectRenderer.material.mainTexture = usableTextures[i];
+            }
+        }
+    }
+
+    private void CheckObject()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectChange = hit.transform.gameObject;
+            objectRenderer = objectChange.GetComponent<Renderer>();
         }
     }
 }
